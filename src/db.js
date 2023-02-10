2c44392798caf391e843841236920df4042cb2c5
ChaeMendoza <israelmendoza656@outlook.com>
2023-02-10 18:07:21 -0500

    Deploy

--- src/db.js
+++ src/db.js
@@ -3,11 +3,11 @@ const { Sequelize } = require('sequelize');
 const makeAssociations = require("./associations/associations");
 const models = require("./fsModels/fsModels"); 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, MODEL_NAME
+    DB_USER, DB_PASSWORD, DB_HOST, MODEL_NAME, DB_DEPLOY
 } = process.env;
 const sequelizePath = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${MODEL_NAME}`;
 
-const sequelize = new Sequelize(sequelizePath, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
