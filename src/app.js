ed4a4eeb9d2f5383d01fec16b7caa8d7800d508e
ChaeMendoza <israelmendoza656@outlook.com>
2023-02-12 22:17:47 -0500

    Fix cors again because not found

--- src/app.js
+++ src/app.js
@@ -3,19 +3,10 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require("cors");
-require('./db.js');
-require("dotenv").config()
 
+require('./db.js');
 
 const server = express();
-
-let corsOptions = {
-    origin: 'https://only-pets-pf.web.app/',
-    optionsSuccessStatus: 200 // For legacy browser support
-}
-
-server.use(cors());
 
 server.name = 'API';
 
@@ -23,23 +14,22 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-/* 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');//ponele 'http://localhost:3000' // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next(); 
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
 });
-*/
+
 server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err, "ES ESTO");
-  res.status(status).send(message);
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
 });
 
 module.exports = server;
